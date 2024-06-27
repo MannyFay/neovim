aa54ede7771f081e6a62fc24e18066e205d129a9
DaniHolzer <holzerdaniel01@gmail.com>
2024-06-27 22:24:04 +0200

    add laravel plugin

---
+++ lua/user/plugins/laravel.lua
@@ -0,0 +1,20 @@
+-------------------------------------------------------------------------------
+-- Laravel Plugin
+-- https://github.com/adalessa/laravel.nvim
+-------------------------------------------------------------------------------
+return{
+    "adalessa/laravel.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "tpope/vim-dotenv",
+        "MunifTanjim/nui.nvim",
+        "nvimtools/none-ls.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+     { "<leader>la", ":Laravel artisan<cr>" },
+     { "<leader>lr", ":Laravel routes<cr>" },
+     { "<leader>lm", ":Laravel related<cr>" },
+    },
+    config = true,
+}
