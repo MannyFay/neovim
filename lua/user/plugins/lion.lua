123c255d9a559c7900b0787d7d853ee8dab6d460
DaniHolzer <holzerdaniel01@gmail.com>
2024-06-27 22:04:15 +0200

    add lion plugin

---
+++ lua/user/plugins/lion.lua
@@ -0,0 +1,32 @@
+-- ~/.dotfiles/nvim/lua/user/plugin_options/lion.lua
+
+-- https://github.com/tommcdo/vim-lion
+
+
+
+------------------------------------------------------------------------------
+-- Vim Lion Plugin
+------------------------------------------------------------------------------
+return{
+    "tommcdo/vim-lion",
+    config = function()
+
+------------------------------------------------------------
+-- Appearance
+
+-- Remove as many spaces as possible when aligning
+vim.g.lion_squeeze_spaces = 1
+
+
+------------------------------------------------------------
+-- Key Mappings
+
+-- Align on the left of the alignment character:
+vim.api.nvim_set_keymap('n', '<Leader>al', '<Plug>(lion-right-align-operator)', { silent = true })
+
+-- Align on the right of the alignment character:
+vim.api.nvim_set_keymap('n', '<Leader>ar', '<Plug>(lion-left-align-operator)', { silent = true })
+
+end,
+}
+
